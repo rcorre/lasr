ca57521ff156947773b1549390a5821b9ad82745
Ryan Roden-Corrent <git@rrc.codes>
2025-07-10 06:51:37 -0400

    Fix async compile

--- src/search.rs
+++ src/search.rs
@@ -1,12 +1,7 @@
-use std::{
-    path::{Path, PathBuf},
-    sync::Arc,
-};
-
 use anyhow::Result;
 use regex::Regex;
-use tokio::sync::Mutex;
-use tokio::sync::mpsc::{Receiver, Sender};
+use std::{path::PathBuf, sync::Arc};
+use tokio::sync::mpsc::Sender;
 use tracing::trace;
 
 pub struct Finding {
@@ -15,31 +10,29 @@ pub struct Finding {
     line: String,
 }
 
-pub async fn search(tx: Sender<Finding>, re: Arc<Mutex<Regex>>, path: PathBuf) {
-    tokio::spawn(async move {
-        search_task(tx, re, path).await.unwrap();
-    });
+pub async fn search(tx: Sender<Finding>, re: Arc<Regex>, path: PathBuf) -> Result<()> {
+    tokio::spawn(async move { search_task(tx, re, path).await }).await?
 }
 
-async fn search_task(tx: Sender<Finding>, re: Arc<Mutex<Regex>>, path: PathBuf) -> Result<()> {
+async fn search_task(tx: Sender<Finding>, re: Arc<Regex>, path: PathBuf) -> Result<()> {
     for entry in std::fs::read_dir(path.as_path())? {
         let entry = entry?;
         let meta = entry.metadata()?;
         let path = entry.path();
         trace!("Exploring path {path:?}: {meta:?}");
         if meta.is_dir() {
-            search(tx.clone(), re.clone(), path.into()).await;
+            Box::pin(search_task(tx.clone(), re.clone(), path)).await?;
         } else if meta.is_file() {
             let contents = std::fs::read_to_string(path.as_path())?;
             for (line_number, line) in contents.lines().enumerate() {
-                if re.lock().await.is_match(&contents) {
+                if re.is_match(line) {
                     trace!("Found match in {path:?}:{line_number}: {line}");
                     tx.send(Finding {
                         path: path.clone(),
                         line_number,
                         line: line.to_string(),
                     })
-                    .await;
+                    .await?;
                 }
             }
         }
